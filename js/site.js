efc746ba7f5137b442fa442d11dc172461cebeb8
Joshua Hysong <jhysong@rhsinc.com>
2019-10-27 14:33:19 -0400

    Updated menu

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 $(document).on('click', 'a[href^="#"]', function(event) {
-    event.preventDefault();  
+    event.preventDefault();
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top
-    }, 1000);
+    }, 500);
   });
